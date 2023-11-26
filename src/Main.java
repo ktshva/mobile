cddc8716a09e43e0dea0abf7d236a3e770402431
Arina Kotysheva <kotysheva.arina@gmail.com>
2023-11-26 19:10:52 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 250; // сумма начального счёта
+        int money = 1300; // сумма пополнения
+        int threshold = 100; // порог для 1 бонуса
+
+        if (money > 1000) {
+            int finalbalance = balance + money; // итоговый баланс после пополнения
+            int bonus = money / threshold; // количество бонусных рублей
+            System.out.println(finalbalance + " итоговый счет");
+            System.out.println(bonus + " бонусных рублей");
+        }
+        else {
+            int finalbalance = balance + money; // итоговый баланс после пополнения
+            int bonus = 0; // количество бонусных рублей
+            System.out.println(finalbalance + " итоговый счет");
+            System.out.println(bonus + " бонусных рублей");
+        }
+
+
+
+
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
